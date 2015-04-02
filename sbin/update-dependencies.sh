89b84efa2daeed3b03bb832e2bc605fd02193982
Dan Kubb <dan.kubb@gmail.com>
2015-04-01 23:17:57 -0700

    Change constraints updates to be atomic
    * The sponge command will slurp in the full contents before writing out
      the full contents of the file.

--- sbin/update-dependencies.sh
+++ sbin/update-dependencies.sh
@@ -8,11 +8,14 @@ IFS=$'\n\t'
 [ -r ./env.sh ] && source env.sh
 
 echo 'Update application dependencies'
-halcyon constraints 2>/dev/null | sort -u > .halcyon/constraints
+halcyon constraints 2>/dev/null \
+  | sort -u \
+  | sponge .halcyon/constraints
 
 echo 'Update sandbox extra app dependencies'
 xargs -I{} sh -c "
   echo 'Update {} dependencies' && \
   halcyon constraints {} 2>/dev/null \
-    | sort -u > .halcyon/sandbox-extra-apps-constraints/{}.constraints
+    | sort -u \
+    | sponge .halcyon/sandbox-extra-apps-constraints/{}.constraints
 " < .halcyon/sandbox-extra-apps
